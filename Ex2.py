d82bc52a391939794f18e61805af3654920045ea
roseCeigler <30753724+roseCeigler@users.noreply.github.com>
2017-08-06 11:55:13 -0400

    Add files via upload

---
+++ Ex2.py
@@ -0,0 +1,10 @@
+# Write a Python program to get the Python version you are using
+# Searched for keywords my guess was
+# print (copyright())
+# print (help())
+
+import sys
+print (sys.version)
+print (sys.version_info)
+
+# You could say I practiced importing packages.

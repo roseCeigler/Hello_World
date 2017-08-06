25e611cb82574250b8a5d640d13a94f348efa938
roseCeigler <30753724+roseCeigler@users.noreply.github.com>
2017-08-06 12:03:12 -0400

    Add files via upload

---
+++ Ex3.py
@@ -0,0 +1,21 @@
+# Write a Python program to display the current date and time.
+# Sample Output :
+# Current date and time :
+# 2014-07-05 14:34:14
+
+import datetime
+print ('Current date and time:')
+# My guess:
+# print (datetime.date, datetime.time)
+# Incorrect output:
+### Current date and time:
+### <class 'datetime.date'> <class 'datetime.time'>
+
+# Second attempt:
+# print (datetime.now)
+
+# W3's Sample:
+# import datetime
+# now = datetime.datetime.now()
+# print ("Current date and time : ")
+# print (now.strftime("%Y-%m-%d %H:%M:%S"))

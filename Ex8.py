b4acb035e8d804117ed1f901ca3a9d30fa9a6cef
roseCeigler <30753724+roseCeigler@users.noreply.github.com>
2017-08-06 13:20:22 -0400

    Add files via upload

---
+++ Ex8.py
@@ -0,0 +1,6 @@
+# Write a Python program to display the first and last colors from the following list.
+
+color_list = ["Red","Green","White" ,"Black"]
+print (color_list)
+print ('First Color: ' + color_list[0] + '\n' +
+        'Last Color: ',color_list[-1])
